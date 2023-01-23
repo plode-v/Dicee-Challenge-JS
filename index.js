1a1216d18f5ac7ae5fa59cd0ac53e74b4aaad67d
Plode <84858705+plode-v@users.noreply.github.com>
2023-01-23 13:28:56 +0700

    added random number generators for both dicee and change images when refresh.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Generate random numbers for dice 1
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-document.querySelector(".img1").setAttribute('src', "iamges/dice"+randomNumber1+".png");
+document.querySelector(".img1").setAttribute('src', "images/dice"+randomNumber1+".png");
 
 // Generate random numbers for dice 2
 var randomNumber2 = Math.floor(Math.random()*6)+1;
