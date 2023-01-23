b5e2b20057d082bd77e787de5530bfd260c1860c
Plode <84858705+plode-v@users.noreply.github.com>
2023-01-23 13:36:31 +0700

    Added if statememts in index.js to check which dice has higher number and change 'refresh me' accordingly.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
 } else {
     document.querySelector('h1').innerHTML = "Draw";
 }
